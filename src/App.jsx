21ea20fcfc708d4d73fc1abf844617b19cfa2a47
shuizhuimiaoman <234304627+shuizhuimiaoman@users.noreply.github.com>
2026-07-01 08:58:48 +0800

    slightly updated site loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
 import React, { useEffect, Suspense } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop.jsx"
-import Home  from "./Home";
+import Home from "./Home";
 import HelloWorld from "./blogposts/HelloWorld";
-
-const blahaj25writeups = () => import("./blogposts/BlahajCTF-2025.jsx");
-const BlahajCTF2025Writeups = React.lazy(() => blahaj25writeups());
-BlahajCTF2025Writeups.preload = blahaj25writeups;
-
-
+import BlahajCTF2025Writeups from "./blogposts/BlahajCTF-2025";
 
 function App() {
-  useEffect(() => {BlahajCTF2025Writeups.preload()}, []);
-
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Router basename={`/${import.meta.env.BASE_URL}`}>
-          <ScrollToTop>
-            <Routes>
-              <Route path="*" element={<Home />} />
-              <Route path="/hello-world" element={<HelloWorld />} />
-              <Route path="/blahajctf-2025-writeups" element={<BlahajCTF2025Writeups />} />
-            </Routes>
-          </ScrollToTop>
-        </Router>
-      </Suspense>
+    <Router basename={`/${import.meta.env.BASE_URL}`}>
+      <ScrollToTop>
+	<Routes>
+	  <Route path="*" element={<Home />} />
+	  <Route path="/hello-world" element={<HelloWorld />} />
+	  <Route path="/blahajctf-2025-writeups" element={<BlahajCTF2025Writeups />} />
+	  <Route path="/test" element={<Test />} />
+	</Routes>
+      </ScrollToTop>
+    </Router>
     </>
   )
 }
